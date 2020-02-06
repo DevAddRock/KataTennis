35c4bb4c7571ac0746b6b1e3323fb17b05dd886e
DevAddRock <46969202+DevAddRock@users.noreply.github.com>
2020-02-06 21:02:43 +0100

    Projet TennisGame
    -Ajout des méthodes:
    *MatchScore
    *IsWin

--- KataTennis/TennisGame/Tie_BreakTennis.cs
+++ KataTennis/TennisGame/Tie_BreakTennis.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace TennisGame
 {
-    class Tie_BreakTennis
+    public class Tie_BreakTennis : TennisBase
     {
+        private const int score_7 = 7;
+
+        /// <summary>
+        /// Get the result of socre
+        /// </summary>
+        /// <returns></returns>
+        public string MatchScore()
+        {
+            if (IsWin())
+                return GetPlayer() + " win the match";
+
+            return string.Format("{0} - {1}", ScorePlayer1, ScorePlayer2);
+        }
+
+        /// <summary>
+        /// Return true if we have a winner
+        /// </summary>
+        /// <returns></returns>
+        public override bool IsWin()
+        {
+            return (ScorePlayer1 >= score_7 && ScorePlayer1 > ScorePlayer2 + 1)
+                || (ScorePlayer2 >= score_7 && ScorePlayer2 > ScorePlayer1 + 1);
+        }
     }
 }
